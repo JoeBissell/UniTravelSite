2fbb30d7fcbcb0a8faa9d9df0cab70aa7c2feac7
JoeBissell <Joe.Bissell@yahoo.co.uk>
2021-04-29 12:00:54 +0100

    mering work

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 
 def index():
     return render_template("index.html")
+
+@app.route("/oscar")
+
+def oscar():
+    return render_template("oscar/index.html")
+
+
 
 app.run(debug = True, port = 5000)
