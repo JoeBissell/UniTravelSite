03b1af3d7fb9861d1439794beed7d875c8099f4e
Suleima <sulayma.abbara99@gmail.com>
2021-05-01 01:03:03 +0100

    coach idk

--- coach.py
+++ coach.py
@@ -5,7 +5,16 @@ import hashlib
 import gc
 from functools import wraps
 
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+
 app = Flask(__name__)   
+
+
 
 
 def get_connection():
@@ -20,9 +29,9 @@ def get_connection():
 def coachhome():       
    return render_template('/suleima/coachhome.html')
 
-@app.route("/coach")
+@app.route("/coach", methods = ['GET', 'POST'])
 def coach():
-            return render_template("/suleima/coach.html")
+        return render_template("/suleima/coach.html")
 
     #conn = get_connection()
     #if conn != None:
@@ -43,4 +52,30 @@ def coach():
     #    print("database connection error")
     #    return("database connection error")
 
-#@app.route("/coachresult")
+@app.route("/coachresult", methods=["GET", "POST"])
+def coachresult():
+    if request.method =='GET':
+        leaving = request.args.get('leaving')
+        if leaving != None:
+            conn = get_connection()
+            if conn != None:
+                if conn.is_connected():
+                    print ('sql connection established')
+                    dbcursor = conn.cursor()
+                    SQL_statement = 'SELECT * FROM coachroutes WHERE leaving = %s'
+                    args = (leaving,)
+                    dbcursor.execute(SQL_statement, args)
+                    print('SELECT statement executed')
+                    rows = dbcursor.fetchall()
+                    dbcursor.close()
+                    conn.close()
+                    return render_template('suleima/coachresult.html', resultset=rows)
+                else:
+                    print('db connect error')
+                    return 'db connect error'
+            else:
+                print('db connect error')
+                return 'db connect error'
+        else: 
+            print('invalid route')
+            return render_template('suleima/coach.html')
