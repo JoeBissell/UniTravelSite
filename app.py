c66eee1b4cf1c969285e52162730b7dfc8fec497
Suleima <sulayma.abbara99@gmail.com>
2021-05-03 14:21:00 +0100

    jfksf

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def bradley():
 
 app.add_url_rule('/coachhome', view_func=coach.coachhome)
 app.add_url_rule('/coach', view_func=coach.coach)
-
+app.add_url_rule('/coachresult', view_func=coach.coachresult)
 
 
 app.run(debug = True, port = 5000)
